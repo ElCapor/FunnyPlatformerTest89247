99775e759662edcc97f835eb299612d7fd5e11c4
ElCapor <104172250+ElCapor@users.noreply.github.com>
2023-10-15 16:16:01 +0200

    woo - anim2xml exporter works

--- FunnyPlatformerTest89247/Utils/UtilsForm.Designer.cs
+++ FunnyPlatformerTest89247/Utils/UtilsForm.Designer.cs
@@ -30,12 +30,26 @@
         {
             this.materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.materialLabel30 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialCard1 = new MaterialSkin.Controls.MaterialCard();
+            this.numericFrameWidth = new System.Windows.Forms.NumericUpDown();
+            this.currentAnimsheetFileLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.openAnimSheetBtn = new MaterialSkin.Controls.MaterialButton();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.materialTabSelector1 = new MaterialSkin.Controls.MaterialTabSelector();
-            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
             this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialTabSelector1 = new MaterialSkin.Controls.MaterialTabSelector();
+            this.frameWidthSelectLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel3 = new MaterialSkin.Controls.MaterialLabel();
+            this.numericFrameHeight = new System.Windows.Forms.NumericUpDown();
+            this.exportAnimBtn = new MaterialSkin.Controls.MaterialButton();
+            this.materialMultiLineTextBox1 = new MaterialSkin.Controls.MaterialMultiLineTextBox();
             this.materialTabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.materialCard1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericFrameWidth)).BeginInit();
             this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericFrameHeight)).BeginInit();
             this.SuspendLayout();
             // 
             // materialTabControl1
@@ -53,6 +67,9 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.materialMultiLineTextBox1);
+            this.tabPage1.Controls.Add(this.materialLabel30);
+            this.tabPage1.Controls.Add(this.materialCard1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -60,6 +77,80 @@
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Animations";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // materialLabel30
+            // 
+            this.materialLabel30.AutoSize = true;
+            this.materialLabel30.Depth = 0;
+            this.materialLabel30.Font = new System.Drawing.Font("Roboto Medium", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.materialLabel30.FontType = MaterialSkin.MaterialSkinManager.fontType.H6;
+            this.materialLabel30.Location = new System.Drawing.Point(62, 3);
+            this.materialLabel30.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel30.Name = "materialLabel30";
+            this.materialLabel30.Size = new System.Drawing.Size(135, 24);
+            this.materialLabel30.TabIndex = 66;
+            this.materialLabel30.Text = "Animation2xml";
+            this.materialLabel30.Click += new System.EventHandler(this.materialLabel30_Click);
+            // 
+            // materialCard1
+            // 
+            this.materialCard1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.materialCard1.Controls.Add(this.exportAnimBtn);
+            this.materialCard1.Controls.Add(this.materialLabel3);
+            this.materialCard1.Controls.Add(this.numericFrameHeight);
+            this.materialCard1.Controls.Add(this.frameWidthSelectLabel);
+            this.materialCard1.Controls.Add(this.numericFrameWidth);
+            this.materialCard1.Controls.Add(this.currentAnimsheetFileLabel);
+            this.materialCard1.Controls.Add(this.openAnimSheetBtn);
+            this.materialCard1.Depth = 0;
+            this.materialCard1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialCard1.Location = new System.Drawing.Point(7, 41);
+            this.materialCard1.Margin = new System.Windows.Forms.Padding(14);
+            this.materialCard1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialCard1.Name = "materialCard1";
+            this.materialCard1.Padding = new System.Windows.Forms.Padding(14);
+            this.materialCard1.Size = new System.Drawing.Size(277, 268);
+            this.materialCard1.TabIndex = 0;
+            // 
+            // numericFrameWidth
+            // 
+            this.numericFrameWidth.Location = new System.Drawing.Point(126, 55);
+            this.numericFrameWidth.Name = "numericFrameWidth";
+            this.numericFrameWidth.Size = new System.Drawing.Size(50, 20);
+            this.numericFrameWidth.TabIndex = 2;
+            // 
+            // currentAnimsheetFileLabel
+            // 
+            this.currentAnimsheetFileLabel.AutoSize = true;
+            this.currentAnimsheetFileLabel.Depth = 0;
+            this.currentAnimsheetFileLabel.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.currentAnimsheetFileLabel.Location = new System.Drawing.Point(81, 18);
+            this.currentAnimsheetFileLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.currentAnimsheetFileLabel.Name = "currentAnimsheetFileLabel";
+            this.currentAnimsheetFileLabel.Size = new System.Drawing.Size(38, 19);
+            this.currentAnimsheetFileLabel.TabIndex = 1;
+            this.currentAnimsheetFileLabel.Text = "File : ";
+            // 
+            // openAnimSheetBtn
+            // 
+            this.openAnimSheetBtn.AutoSize = false;
+            this.openAnimSheetBtn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.openAnimSheetBtn.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.openAnimSheetBtn.Depth = 0;
+            this.openAnimSheetBtn.HighEmphasis = true;
+            this.openAnimSheetBtn.Icon = null;
+            this.openAnimSheetBtn.Location = new System.Drawing.Point(3, 10);
+            this.openAnimSheetBtn.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.openAnimSheetBtn.MouseState = MaterialSkin.MouseState.HOVER;
+            this.openAnimSheetBtn.Name = "openAnimSheetBtn";
+            this.openAnimSheetBtn.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.openAnimSheetBtn.Size = new System.Drawing.Size(70, 35);
+            this.openAnimSheetBtn.TabIndex = 0;
+            this.openAnimSheetBtn.Text = "Open";
+            this.openAnimSheetBtn.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.openAnimSheetBtn.UseAccentColor = false;
+            this.openAnimSheetBtn.UseVisualStyleBackColor = true;
+            this.openAnimSheetBtn.Click += new System.EventHandler(this.openAnimSheetBtn_Click);
             // 
             // tabPage2
             // 
@@ -73,31 +164,6 @@
             this.tabPage2.Text = "About";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // materialTabSelector1
-            // 
-            this.materialTabSelector1.BaseTabControl = this.materialTabControl1;
-            this.materialTabSelector1.CharacterCasing = MaterialSkin.Controls.MaterialTabSelector.CustomCharacterCasing.Normal;
-            this.materialTabSelector1.Depth = 0;
-            this.materialTabSelector1.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.materialTabSelector1.Location = new System.Drawing.Point(6, 68);
-            this.materialTabSelector1.MouseState = MaterialSkin.MouseState.HOVER;
-            this.materialTabSelector1.Name = "materialTabSelector1";
-            this.materialTabSelector1.Size = new System.Drawing.Size(781, 27);
-            this.materialTabSelector1.TabIndex = 1;
-            this.materialTabSelector1.Text = "materialTabSelector1";
-            // 
-            // materialLabel1
-            // 
-            this.materialLabel1.AutoSize = true;
-            this.materialLabel1.Depth = 0;
-            this.materialLabel1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.materialLabel1.Location = new System.Drawing.Point(7, 7);
-            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
-            this.materialLabel1.Name = "materialLabel1";
-            this.materialLabel1.Size = new System.Drawing.Size(195, 19);
-            this.materialLabel1.TabIndex = 0;
-            this.materialLabel1.Text = "FunnyPlatformerTools Utils";
-            // 
             // materialLabel2
             // 
             this.materialLabel2.AutoSize = true;
@@ -110,6 +176,96 @@
             this.materialLabel2.TabIndex = 1;
             this.materialLabel2.Text = "A simple tool to make my life easier to make simple games";
             // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialLabel1.Location = new System.Drawing.Point(7, 7);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(195, 19);
+            this.materialLabel1.TabIndex = 0;
+            this.materialLabel1.Text = "FunnyPlatformerTools Utils";
+            // 
+            // materialTabSelector1
+            // 
+            this.materialTabSelector1.BaseTabControl = this.materialTabControl1;
+            this.materialTabSelector1.CharacterCasing = MaterialSkin.Controls.MaterialTabSelector.CustomCharacterCasing.Normal;
+            this.materialTabSelector1.Depth = 0;
+            this.materialTabSelector1.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialTabSelector1.Location = new System.Drawing.Point(-1, 68);
+            this.materialTabSelector1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabSelector1.Name = "materialTabSelector1";
+            this.materialTabSelector1.Size = new System.Drawing.Size(801, 27);
+            this.materialTabSelector1.TabIndex = 1;
+            this.materialTabSelector1.Text = "materialTabSelector1";
+            // 
+            // frameWidthSelectLabel
+            // 
+            this.frameWidthSelectLabel.AutoSize = true;
+            this.frameWidthSelectLabel.Depth = 0;
+            this.frameWidthSelectLabel.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.frameWidthSelectLabel.Location = new System.Drawing.Point(17, 55);
+            this.frameWidthSelectLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.frameWidthSelectLabel.Name = "frameWidthSelectLabel";
+            this.frameWidthSelectLabel.Size = new System.Drawing.Size(91, 19);
+            this.frameWidthSelectLabel.TabIndex = 3;
+            this.frameWidthSelectLabel.Text = "Frame Width";
+            // 
+            // materialLabel3
+            // 
+            this.materialLabel3.AutoSize = true;
+            this.materialLabel3.Depth = 0;
+            this.materialLabel3.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialLabel3.Location = new System.Drawing.Point(17, 84);
+            this.materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel3.Name = "materialLabel3";
+            this.materialLabel3.Size = new System.Drawing.Size(96, 19);
+            this.materialLabel3.TabIndex = 5;
+            this.materialLabel3.Text = "Frame Height";
+            // 
+            // numericFrameHeight
+            // 
+            this.numericFrameHeight.Location = new System.Drawing.Point(126, 84);
+            this.numericFrameHeight.Name = "numericFrameHeight";
+            this.numericFrameHeight.Size = new System.Drawing.Size(50, 20);
+            this.numericFrameHeight.TabIndex = 4;
+            // 
+            // exportAnimBtn
+            // 
+            this.exportAnimBtn.AutoSize = false;
+            this.exportAnimBtn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.exportAnimBtn.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.exportAnimBtn.Depth = 0;
+            this.exportAnimBtn.HighEmphasis = true;
+            this.exportAnimBtn.Icon = null;
+            this.exportAnimBtn.Location = new System.Drawing.Point(3, 224);
+            this.exportAnimBtn.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.exportAnimBtn.MouseState = MaterialSkin.MouseState.HOVER;
+            this.exportAnimBtn.Name = "exportAnimBtn";
+            this.exportAnimBtn.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.exportAnimBtn.Size = new System.Drawing.Size(265, 35);
+            this.exportAnimBtn.TabIndex = 6;
+            this.exportAnimBtn.Text = "Export";
+            this.exportAnimBtn.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.exportAnimBtn.UseAccentColor = false;
+            this.exportAnimBtn.UseVisualStyleBackColor = true;
+            // 
+            // materialMultiLineTextBox1
+            // 
+            this.materialMultiLineTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.materialMultiLineTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.materialMultiLineTextBox1.Depth = 0;
+            this.materialMultiLineTextBox1.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialMultiLineTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialMultiLineTextBox1.Location = new System.Drawing.Point(302, 41);
+            this.materialMultiLineTextBox1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialMultiLineTextBox1.Name = "materialMultiLineTextBox1";
+            this.materialMultiLineTextBox1.Size = new System.Drawing.Size(460, 268);
+            this.materialMultiLineTextBox1.TabIndex = 67;
+            this.materialMultiLineTextBox1.Text = "";
+            // 
             // UtilsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -121,8 +277,14 @@
             this.Text = "FunnyPlatformerTest Utilities";
             this.Load += new System.EventHandler(this.UtilsForm_Load);
             this.materialTabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.materialCard1.ResumeLayout(false);
+            this.materialCard1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericFrameWidth)).EndInit();
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericFrameHeight)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -135,6 +297,16 @@
         private MaterialSkin.Controls.MaterialTabSelector materialTabSelector1;
         private MaterialSkin.Controls.MaterialLabel materialLabel1;
         private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private MaterialSkin.Controls.MaterialLabel materialLabel30;
+        private MaterialSkin.Controls.MaterialCard materialCard1;
+        private MaterialSkin.Controls.MaterialButton openAnimSheetBtn;
+        private MaterialSkin.Controls.MaterialLabel currentAnimsheetFileLabel;
+        private System.Windows.Forms.NumericUpDown numericFrameWidth;
+        private MaterialSkin.Controls.MaterialLabel materialLabel3;
+        private System.Windows.Forms.NumericUpDown numericFrameHeight;
+        private MaterialSkin.Controls.MaterialLabel frameWidthSelectLabel;
+        private MaterialSkin.Controls.MaterialButton exportAnimBtn;
+        private MaterialSkin.Controls.MaterialMultiLineTextBox materialMultiLineTextBox1;
     }
 }
 
--- FunnyPlatformerTest89247/Utils/UtilsForm.cs
+++ FunnyPlatformerTest89247/Utils/UtilsForm.cs
@@ -9,6 +9,10 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using MaterialSkin.Controls;
 using MaterialSkin;
+using System.IO;
+using System.Security;
+using System.Xml;
+
 namespace Utils
 {
     public partial class UtilsForm : MaterialForm
@@ -33,6 +37,83 @@ namespace Utils
 
         private void UtilsForm_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void materialLabel30_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openAnimationSheetBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private XmlDocument processImage(string filePath)
+        {
+            try
+            {
+                using (Bitmap image = new Bitmap(filePath))
+                {
+                    int frameCount = image.Width / (int)this.numericFrameWidth.Value;
+                    this.materialMultiLineTextBox1.AppendText($"There is {frameCount} frames \n");
+
+                    XmlDocument xmlDocument = new XmlDocument();
+                    XmlElement rootElement = xmlDocument.CreateElement("AnimationSheet");
+                    rootElement.SetAttribute("file", Path.GetFileName(filePath));
+                    xmlDocument.AppendChild(rootElement);
+
+                    for (int frameId = 0; frameId < frameCount; frameId++)
+                    {
+                        XmlElement frameElement = xmlDocument.CreateElement("Frame");
+                        frameElement.SetAttribute("id", frameId.ToString());
+                        frameElement.SetAttribute("x", (frameId * this.numericFrameWidth.Value).ToString());
+                        frameElement.SetAttribute("y", "0");
+                        frameElement.SetAttribute("width", ((int)this.numericFrameWidth.Value).ToString());
+                        frameElement.SetAttribute("height", ((int)this.numericFrameHeight.Value).ToString());
+
+                        rootElement.AppendChild(frameElement);
+                    }
+
+                    this.materialMultiLineTextBox1.AppendText(xmlDocument.OuterXml);
+                    return xmlDocument;
+                }
+                
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+                return new XmlDocument();
+            }
+        }
+        private void openAnimSheetBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PNG Files|*.png";
+            openFileDialog.Title = "Select a png file";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFileName = openFileDialog.FileName;
+                this.materialMultiLineTextBox1.AppendText(selectedFileName);
+
+                XmlDocument doc = processImage(selectedFileName);
+
+
+                this.exportAnimBtn.Click += (sendr, args) =>
+                    {
+                        SaveFileDialog saveFileDialog = new SaveFileDialog();
+                        saveFileDialog.Filter = "XML Files|*.xml";
+                        saveFileDialog.Title = "Save xml file";
+
+                        if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                        {
+                            doc.Save(saveFileDialog.FileName);
+                        }
+                    };
+            }
+            
 
         }
     }
